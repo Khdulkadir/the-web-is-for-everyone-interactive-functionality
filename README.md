4ff0d1a9b580e5d1aafeeddda228d51de8d2aadf
Khdulkadir <khdulkadir@gmail.com>
2024-03-21 15:17:02 +0100

    .

--- README.md
+++ README.md
@@ -1,27 +1,19 @@
 > _Fork_ deze leertaak en ga aan de slag. Onderstaande outline ga je gedurende deze taak in jouw eigen GitHub omgeving uitwerken. De instructie vind je in: [docs/INSTRUCTIONS.md](docs/INSTRUCTIONS.md)
 
-# Titel
+# Red Pers
+Omzetten van nieuwssite gemaakt met elementor naar ejs en node.
 <!-- Geef je project een titel en schrijf in één zin wat het is -->
+![image](https://github.com/Khdulkadir/server-side-rendering-server-side-website/assets/144004145/1dba716c-aed1-4173-ac49-902bd3bdcd5f)
 
-## Inhoudsopgave
-
-  * [Beschrijving](#beschrijving)
-  * [Gebruik](#gebruik)
-  * [Kenmerken](#kenmerken)
-  * [Installatie](#installatie)
-  * [Bronnen](#bronnen)
-  * [Licentie](#licentie)
 
 ## Beschrijving
-<!-- Bij Beschrijving staat kort beschreven wat voor project het is en wat je hebt gemaakt -->
-<!-- Voeg een mooie poster visual toe 📸 -->
-<!-- Voeg een link toe naar Github Pages 🌐-->
+Na het forken van de repo heb ik eerst ik alle vereiste bestanden aangemaakt in VS Code. Server.js voor de backend, /views voor de ejs code, /public voor alle assets. Daarna heb ik npm install ingevoerd in de console om Node modules te installeren. Alle backend code heb ik overgenomen van de vorige sprint voor zover dat mogelijk was.
 
 ## Gebruik
-<!-- Bij Gebruik staat de user story, hoe het werkt en wat je er mee kan. -->
+<!--Bij Gebruik staat hoe je project er uit ziet, hoe het werkt en wat je er mee kan. -->
 
 ## Kenmerken
-<!-- Bij Kenmerken staat welke technieken zijn gebruikt en hoe. Wat is de HTML structuur? Wat zijn de belangrijkste dingen in CSS? Wat is er met JS gedaan en hoe? Misschien heb je iets met NodeJS gedaan, of heb je een framwork of library gebruikt? -->
+<!-- Bij Kenmerken staat welke technieken zijn gebruikt en hoe. Wat is de HTML structuur? Wat zijn de belangrijkste dingen in CSS? Wat is er met Javascript gedaan en hoe? Misschien heb je een framwork of library gebruikt? -->
 
 ## Installatie
 <!-- Bij Instalatie staat hoe een andere developer aan jouw repo kan werken -->
--- server.js
+++ server.js
@@ -1,1 +1,106 @@
-console.log('Hier komt je server')
+/*** Express setup & start ***/
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from "./helpers/fetch-json.js";
+
+// Importeer het npm pakket express uit de node_modules map
+import express, { response } from "express";
+
+// Maak een nieuwe express app aan
+const app = express();
+
+// Stel ejs in als template engine
+app.set("view engine", "ejs");
+
+// Stel de map met ejs templates in
+app.set("views", "./views");
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static("public"));
+
+// Zorgt dat werken met request data makkelijker wordt
+app.use(express.urlencoded({ extended: true }));
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set("port", process.env.PORT || 8000);
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get("port"), function () {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+/*** Constants ***/
+
+const baseURL = "https://redpers.nl/wp-json/wp/v2/posts?categories=";
+const perPage = "&per_page=3";
+
+const apiURLVoorpagina = "https://redpers.nl/wp-json/wp/v2/posts?per_page=4";
+const apiURLBinnenland = baseURL + 9 + perPage;
+const apiURLBuitenland = baseURL + 1010 + perPage;
+const apiURLColumns = baseURL + 10 + perPage;
+const apiURLEconomie = baseURL + 6 + perPage;
+const apiURLKunstMedia = baseURL + 4 + perPage;
+const apiURLPodcast = baseURL + 3211 + perPage;
+const apiURLPolitiek = baseURL + 63 + perPage;
+const apiURLWetenschap = baseURL + 94 + perPage;
+
+/*** Routes & data ***/
+
+app.get("/", function (request, response) {
+  Promise.all([
+    fetchJson(apiURLVoorpagina),
+    fetchJson(apiURLBinnenland),
+    fetchJson(apiURLBuitenland),
+    fetchJson(apiURLColumns),
+    fetchJson(apiURLEconomie),
+    fetchJson(apiURLKunstMedia),
+    fetchJson(apiURLPodcast),
+    fetchJson(apiURLPolitiek),
+    fetchJson(apiURLWetenschap),
+  ]).then(
+    ([
+      voorpaginaData,
+      binnenlandData,
+      buitenlandData,
+      columnsData,
+      economieData,
+      kunstmediaData,
+      podcastData,
+      politiekData,
+      wetenschapData,
+    ]) => {
+      response.render("index", {
+        voorpagina: voorpaginaData,
+        binnenland: binnenlandData,
+        buitenland: buitenlandData,
+        columns: columnsData,
+        economie: economieData,
+        kunstmedia: kunstmediaData,
+        podcast: podcastData,
+        politiek: politiekData,
+        wetenschap: wetenschapData,
+      });
+    }
+  );
+});
+
+app.get("/:categories", function (request, response) {
+  fetchJson(
+    `https://redpers.nl/wp-json/wp/v2/posts?categories=${request.params.categories}`
+  ).then((categoryData) => {
+    response.render("category", {
+      category: categoryData,
+    });
+  });
+});
+
+app.get("/:categories/:id", function (request, response) {
+  fetchJson(`https://redpers.nl/wp-json/wp/v2/posts/${request.params.id}`).then(
+    (articleData) => {
+      response.render("article", {
+        article: articleData,
+      });
+    }
+  );
+});
